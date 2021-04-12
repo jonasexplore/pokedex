a40d3e02c1aec7b9656ec6b6c903950b0a8e6a90
Jonas Brito <jonasexplore@gmail.com>
2021-04-12 00:12:25 -0300

    refactory to recive data.

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,7 +1,9 @@
 import { useContext } from "react";
 import styled from "styled-components";
-import { dataContext } from "../contexts/DataContext";
-import Card from "./Card/";
+import { DataContext } from "../contexts/DataContext";
+import { ModalContext } from "../contexts/ModalContext";
+import Card from "./Cards";
+import Details from "./Details";
 
 const Container = styled.div`
   display: flex;
@@ -9,17 +11,29 @@ const Container = styled.div`
 
   align-items: center;
   justify-content: center;
+
+  margin-bottom: 2rem;
+`;
+
+const Description = styled.p`
+  margin-top: 2rem;
 `;
 
 const CardList = () => {
-  const { pokemons } = useContext(dataContext);
+  const { filteredPokemons } = useContext(DataContext);
+  const { isModalActive } = useContext(ModalContext);
 
-  return (
-    <Container>
-      {pokemons.map((pokemon, index) => (
-        <Card key={index} name={pokemon.name} url={pokemon.url} />
-      ))}
-    </Container>
+  return !filteredPokemons.length ? (
+    <Description>Nenhum pokémon foi encontrado :(</Description>
+  ) : (
+    <>
+      <Container>
+        {filteredPokemons.map((pokemon, index) => (
+          <Card key={index} {...pokemon} />
+        ))}
+      </Container>
+      {isModalActive && <Details />}
+    </>
   );
 };
 
