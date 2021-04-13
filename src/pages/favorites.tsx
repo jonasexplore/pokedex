98c0299a45976f297215d013d1e7ac8a3a637e7b
fallying <jonasexplore@gmail.com>
2021-04-13 17:22:41 -0300

    fix color background

--- src/pages/favorites.tsx
+++ src/pages/favorites.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link";
 import { useContext } from "react";
 import styled from "styled-components";
+import DefaultButton from "../components/Buttons/DefaultButton";
 import CardList from "../components/Cards/CardList";
 import Container from "../components/Container";
 import Navbar from "../components/Navbar";
@@ -10,6 +12,14 @@ const ContentCenter = styled(Container)`
   flex-flow: column;
   align-items: center;
   justify-content: center;
+
+  h2 {
+    color: ${(props) => props.theme.colors.text};
+  }
+
+  p {
+    margin: 1rem 0;
+  }
 `;
 
 const Content = styled(Container)`
@@ -21,7 +31,7 @@ const Content = styled(Container)`
 
 const Description = styled.h2`
   margin-top: 2rem;
-  color: var(--black);
+  color: ${(props) => props.theme.colors.text};
 `;
 
 const Favorites = () => {
@@ -34,6 +44,9 @@ const Favorites = () => {
           <img src="images/Astronaut.png" alt="" />
           <h2>Está meio vazio por aqui!</h2>
           <p>Procure por pokémons para adicioná-los aos seus favoritos.</p>
+          <Link href="/search">
+            <DefaultButton>Procurar pokémons</DefaultButton>
+          </Link>
         </ContentCenter>
       ) : (
         <Content>
